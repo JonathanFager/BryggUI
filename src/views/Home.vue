6ceb45edfd0e0f67828bfecc5cebf4b8bbbc90a2
sCarlman <carl.albertsson@gmail.com>
2020-12-19 20:46:30 +0100

    test commit carl

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1097,6 +1097,7 @@
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 // import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+// test commit TODO: remove
 
 @Options({
   components: {},
